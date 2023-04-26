8c5979ecf2a02bc23f42ba3ecc24929eb0f7e752
Your Name <you@example.com>
2023-04-26 12:10:32 +0200

    usunełam niepotrzebne if

--- skrypt.py
+++ skrypt.py
@@ -90,21 +90,7 @@ class Transformacje:
         t = np.tan(f)
         e_2 = self.ecc2/(1-self.ecc2)
         n2 = e_2 * (np.cos(f))**2
-        '''
-        l = np.rad2deg(l)
-        if l>13.5 and l <16.5:
-            s = 5
-            l0 = 15
-        elif l>16.5 and l <19.5:
-            s = 6
-            l0 = 18
-        elif l>19.5 and l <22.5:
-            s = 7
-            l0 = 21
-        elif l>22.5 and l <25.5:
-            s = 8
-            l0 = 24
-        '''
+       
         l= np.deg2rad(l)
         l0 =np.deg2rad(l0)
         d_l = l - l0
@@ -147,9 +133,9 @@ class Transformacje:
         x92 = m*xgk - 5300000
         y92 = m*ygk + 500000
         return(x92, y92, xgk, ygk)
-    
-print()
-print("XYZ2BLH")
+
+
+print("transformacje")
 #XYZ2BLH   
 if __name__ == "__main__":
     transformator_wgs84 = Transformacje("wgs84")
@@ -158,9 +144,7 @@ if __name__ == "__main__":
     print(f, l, h)
     (dms(f))
     (dms(l))
-         
     
-if __name__ == "__main__":
     transformator_grs80 = Transformacje("grs80")
     X = 3664940.500; Y = 1409153.590; Z = 5009571.170
     f, l, h = transformator_grs80.transform_XYZ2BLH(X,Y,Z)
@@ -168,7 +152,6 @@ if __name__ == "__main__":
     (dms(f))
     (dms(l))
 
-if __name__ == "__main__":
     transformator_Krasowskiego = Transformacje("Krasowskiego")
     X = 3664940.500; Y = 1409153.590; Z = 5009571.170
     f, l, h = transformator_Krasowskiego.transform_XYZ2BLH(X,Y,Z)
@@ -176,87 +159,61 @@ if __name__ == "__main__":
     (dms(f))
     (dms(l))
     
-print()
-print("BLH2XYZ")
-#BLH2XYZ
-if __name__ == "__main__":
     transformator_wgs84 = Transformacje("wgs84")
     f = 3664940.500; l = 1409153.590; h = 5009571.170
     X,Y,Z = transformator_wgs84.transform_BLH2XYZ(f,l,h)
     print(X,Y,Z)
     
-if __name__ == "__main__":
     transformator_grs80 = Transformacje("grs80")
     f = 3664940.500; l = 1409153.590; h = 5009571.170
     X,Y,Z = transformator_grs80.transform_BLH2XYZ(f,l,h)
     print(X,Y,Z)
     
-    
-if __name__ == "__main__":
     transformator_Krasowskiego = Transformacje("Krasowskiego")
     f = 3664940.500; l = 1409153.590; h = 5009571.170
     X,Y,Z = transformator_Krasowskiego.transform_BLH2XYZ(f,l,h)
     print(X,Y,Z)
-
-print()
-print("XYX2neu")
-#XYX2neu  
-if __name__ == "__main__":
+    
     transformator_wgs84 = Transformacje("wgs84")
     xa = 15445; ya = 1544; za = 45541; xb = 451; yb = 154; zb = 154; phi = 17; lam= 179; h = 100
     N, E, U, phi_stopnie, lam_stopnie = transformator_wgs84.transform_XYZ2neu(xa, ya, za, xb, yb, zb, phi, lam, h)
     print(N, E, U, phi_stopnie, lam_stopnie)
     
-if __name__ == "__main__":
     transformator_grs80 = Transformacje("grs80")
     xa = 1; ya = 1; za = 1; xb = 1; yb = 1; zb = 1; phi = 1; lam= 1; h = 1
     N, E, U, phi_stopnie, lam_stopnie = transformator_grs80.transform_XYZ2neu(xa, ya, za, xb, yb, zb, phi, lam, h)
     print(N, E, U, phi_stopnie, lam_stopnie)
     
-if __name__ == "__main__":
     transformator_Krasowskiego = Transformacje("Krasowskiego")
     xa = 1; ya = 1; za = 1; xb = 1; yb = 1; zb = 1; phi = 1; lam= 1; h = 1
     N, E, U, phi_stopnie, lam_stopnie = transformator_Krasowskiego.transform_XYZ2neu(xa, ya, za, xb, yb, zb, phi, lam, h)
     print(N, E, U, phi_stopnie, lam_stopnie)
-
-print()
-print("fl22000")
-#fl2000
-if __name__ == "__main__":
+    
     transformator_wgs84 = Transformacje("wgs84")
     f = 9; l = 16; l0= 15; s =5
     x00, y00,xgk,ygk = transformator_wgs84.transform_u2000(f, l, l0, s)
     print(x00, y00,xgk,ygk)
     
-if __name__ == "__main__":
     transformator_grs80 = Transformacje("grs80")
     f = 9; l = 16; l0= 15; s=5
     x00, y00,xgk,ygk = transformator_grs80.transform_u2000(f, l, l0,s )
     print(x00, y00,xgk,ygk)
     
-if __name__ == "__main__":
     transformator_Krasowskiego = Transformacje("Krasowskiego")
     f = 9; l = 14; l0= 15; s=5
     x00, y00,xgk,ygk = transformator_Krasowskiego.transform_u2000(f, l, l0,s)
     print(x00, y00,xgk,ygk)
     
-print()
-print("fl21992")
- #fl292
-if __name__ == "__main__":
     transformator_wgs84 = Transformacje("wgs84")
     f = 9; l = 14
     x92, y92,xgk,ygk = transformator_wgs84.transform_u1992(f, l)
     print(x92, y92, xgk, ygk)
-
-if __name__ == "__main__":
+    
     transformator_grs80 = Transformacje("grs80")
     f = 9; l = 14
     x92, y92,xgk,ygk = transformator_grs80.transform_u1992(f, l)
     print(x92, y92, xgk, ygk)
     
-
-if __name__ == "__main__":
     transformator_Krasowskiego = Transformacje("Krasowskiego")
     f = 9; l = 14
     x92, y92,xgk,ygk = transformator_Krasowskiego.transform_u1992(f, l)
